4710357772e14e90d8f5ec9f5564369936b64f82
Abd2lrady <abd2lrady@gmail.com>
2022-06-10 18:31:56 +0200

    adding constant enum for url resources

---
+++ iOS_ProductTask/Resources/Constants/Api/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  ApiConstants.swift
+//  iOS_ProductTask
+//
+//  Created by Ahmad Abdulrady
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum Api {
+    
+        static let baseURL = "https://api.jsonserve.com"
+        static let path = "/3wPBqi"
+    }
+    
+}
