f0e01cd0a87b0b0f7c1c12826444ef4f46bb22e0
Abd2lrady <abd2lrady@gmail.com>
2022-06-10 19:18:08 +0200

    adding product model

--- iOS_ProductTask/Scenes/ViewController.swift
+++ iOS_ProductTask/Scenes/ViewController.swift
@@ -13,8 +13,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
+        test()
     }
 
-
+    func test() {
+        let url = URL(string: Constants.Api.baseURL + Constants.Api.path)!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            do {
+                
+                let products =  try JSONDecoder().decode([Product].self, from: data)
+                print(products)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
 }
 
