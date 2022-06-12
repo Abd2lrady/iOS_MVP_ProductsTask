a9200a5bebb84772ac5cbdf7765e911b925c37ee
Abd2lrady <abd2lrady@gmail.com>
2022-06-12 09:14:11 +0200

    Adding ProductService

---
+++ iOS_ProductTask/Service/Product/ProductService.swift
@@ -0,0 +1,17 @@
+//
+//  ProductService.swift
+//  iOS_ProductTask
+//
+//  Created by Ahmad Abdulrady
+//
+
+import Foundation
+
+class ProductService: ProductGateway {
+    
+    func getProducts(completionHandler: @escaping (Result<[Product], Error>) -> Void) {
+        
+    }
+    
+    
+}
---
+++ iOS_ProductTask/Service/Product/RemoteProduct/RemoteProductService.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteProductService.swift
+//  iOS_ProductTask
+//
+//  Created by Ahmad Abdulrady
+//
+
+import Foundation
+
+class RemoteProductService: ProductGateway {
+    func getProducts(completionHandler: @escaping (Result<[Product], Error>) -> Void) {
+        
+    }
+    
+    
+}
