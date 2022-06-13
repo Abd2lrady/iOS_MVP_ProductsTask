0c48be7bcb64e17ec8f61a2fe54db1737b0ec5ef
Abd2lrady <abd2lrady@gmail.com>
2022-06-13 22:49:28 +0200

    adding navigationBar UI

--- iOS_ProductTask/Scenes/ProductDetails/ViewController/ProductDetailsVC.swift
+++ iOS_ProductTask/Scenes/ProductDetails/ViewController/ProductDetailsVC.swift
@@ -21,4 +21,28 @@ class ProductDetailsVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configNavBar()
+    }
+    
+    private func configNavBar() {
+        self.hideNavBar()
+        let back = UIButton()
+        let icon = UIImage(named: Assets.navBackButton)
+        back.setImage(icon, for: .normal)
+        back.addTarget(self, action: #selector(backButtonAction), for: .touchUpInside)
+        let backItem = UIBarButtonItem(customView: back)
+        self.navigationItem.leftBarButtonItem = backItem
+    }
+    
+    private func hideNavBar() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.isTranslucent = true
+    }
+    
+    @objc
+    private func backButtonAction() {
+        print("hello")
+    }
 }
