8f23961bcd5330046ac1390b8d20f0611986293f
Abd2lrady <abd2lrady@gmail.com>
2022-06-13 21:27:18 +0200

    Adding UIDesign

---
+++ iOS_ProductTask/Scenes/ProductDetails/ViewController/ProductDetailsVC.swift
@@ -0,0 +1,21 @@
+//
+//  ProductDetailsVC.swift
+//  iOS_ProductTask
+//
+//  Created by Ahmad Abdulrady
+//
+
+import UIKit
+
+class ProductDetailsVC: UIViewController {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
